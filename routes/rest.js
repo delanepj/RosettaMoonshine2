08a75dbd0af5ccaec069c255adeaf3ebd03861e0
Patrick Delaney <pdelaney5@publicisgroupe.net>
2016-04-01 09:40:54 -0400

    Added Skills Rest Service

--- routes/rest.js
+++ routes/rest.js
@@ -16,6 +16,9 @@ module.exports = router;
 
 var fullTimeEmployeeQuery = "select first_name,last_name,level,discipline,location,career_manager,email_address,full_time,skill_name,description from Employee e inner join EmployeeToSkill ets inner join Skill s where e.employee_id = ets.employee_id and s.skill_id = ets.skill_id';"
 
+var getEmployeesWithSkillsQuery="select first_name,last_name,s.skill_name from Employee e inner join EmployeeToSkill ets inner join Skill s where ets.employee_id = e.employee_id and s.skill_id = ets.skill_id and skill_name ="
+
+
 function getAllEmployees(req,res) {
     
     pool.getConnection(function(err,connection){
@@ -65,6 +68,34 @@ function getAllEmployeesAndSkills(req,res) {
 	  });
 }
 
+function getEmployeesWithSkills(req,res){
+	var localQuery = getEmployeesWithSkillsQuery;
+	localQuery += "'" + req.query.skill + "'"	
+	console.log(localQuery)
+	pool.getConnection(function(err,connection){
+			if (err) {
+			  connection.release();
+			  res.json({"code" : 100, "status" : "Error in connection database"});
+			  return;
+			}   
+
+			console.log('connected as id ' + connection.threadId);
+			
+			connection.query(localQuery,function(err,rows){
+				connection.release();
+				if(!err) {
+					res.json(rows);
+				}           
+			});
+
+			connection.on('error', function(err) {      
+				  res.json({"code" : 100, "status" : "Error in connection database"});
+				  return;     
+			});
+	  });
+
+}
+
 
 /* GET All Employees and Skills */
 router.get('/getAllEmployeesAndSkills', function(req, res, next) {
@@ -74,5 +105,9 @@ router.get('/getAllEmployeesAndSkills', function(req, res, next) {
 /* Get All Employees */
 router.get('/getAllEmployees', function(req, res, next) {
           getAllEmployees(req,res);
+});
+
+router.get('/getEmployeesWithSkills', function(req, res, next) {
+          getEmployeesWithSkills(req,res);
 });
 
